ebd2648ead4e1fd9d385e78d543939a475eba0e7
piyushjanawadkar <piyush.janawadkar@gmail.com>
2017-09-30 11:39:21 -0700

    parse command line to generate input/output locations

--- src/main/java/com/computer/nand2tetris/compiler/io/IOPathsCreator.java
+++ src/main/java/com/computer/nand2tetris/compiler/io/IOPathsCreator.java
@@ -1,14 +1,93 @@
 package com.computer.nand2tetris.compiler.io;
 
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.collect.MoreCollectors.onlyElement;
+
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class IOPathsCreator {
 
+  private static final String PARSER_OUTPUT_XML_SUFFIX = ".xml";
+  private static final String TOKENIZER_OUTPUT_XML_SUFFIX = "T.xml";
+  private static final Pattern JACK_FILE_EXTENSION_PATTERN = Pattern.compile("[.]jack$");
+  private static final String OUTPUT_SUBDIR = "parseroutput";
+
   public static ImmutableList<IOPaths> createPaths(String[] args) {
-    return ImmutableList.of(
-        IOPaths.create(
-            "/tmp/inputclass.jack",
-            "/tmp/outputT.xml",
-            "/tmp/output.xml"));
+    String inputLocation = extractInputLocation(args);
+    File file = new File(inputLocation);
+    if (file.isDirectory()) {
+      return createPathsFromDirectory(file);
+    }
+
+    if (file.isFile() && hasJackExtension(file.getPath())) {
+      return ImmutableList.of(createPathsFromFile(file));
+    }
+
+    throw new RuntimeException(
+        "Input location " + inputLocation + " is neither a .jack file nor a directory.");
+  }
+
+  private static ImmutableList<IOPaths> createPathsFromDirectory(File inputDirectory) {
+    return Arrays.stream(
+        inputDirectory.listFiles((dir, name) -> {
+          return hasJackExtension(name);
+        }))
+        .map(IOPathsCreator::createPathsFromFile)
+        .collect(toImmutableList());
+  }
+
+
+  private static boolean hasJackExtension(String path) {
+    return JACK_FILE_EXTENSION_PATTERN.matcher(path).find();
+  }
+
+  private static IOPaths createPathsFromFile(File inputFile) {
+    String parentPath = inputFile.getParent();
+    String outputSubdirectory = createSubdirectory(parentPath, OUTPUT_SUBDIR);
+    return IOPaths.create(
+        inputFile.getAbsolutePath(),
+        createXmlOutputPath(outputSubdirectory, generateTokenizerOutputBasename(inputFile)),
+        createXmlOutputPath(outputSubdirectory, generateParserOutputBasename(inputFile)));
+  }
+
+  private static String createSubdirectory(String parentPath, String outputSubdir) {
+    Path outputSubdirPath = Paths.get(parentPath, outputSubdir);
+    File file = new File(outputSubdirPath.toString());
+    if (!file.exists()) {
+      Preconditions.checkState(file.mkdir(), "output directory %s creation failed.",
+          file.getAbsolutePath());
+    }
+    return outputSubdirPath.toString();
+  }
+
+  private static String generateTokenizerOutputBasename(File inputFile) {
+    return replaceSuffix(inputFile.toPath().getFileName(), TOKENIZER_OUTPUT_XML_SUFFIX);
+  }
+
+  private static String createXmlOutputPath(String outputSubdirectory, String baseName) {
+    return Paths.get(outputSubdirectory, baseName).toAbsolutePath().toString();
+  }
+
+  private static String generateParserOutputBasename(File inputFile) {
+    return replaceSuffix(inputFile.toPath().getFileName(), PARSER_OUTPUT_XML_SUFFIX);
+  }
+
+  private static String replaceSuffix(Path fileName, String parserOutputXmlSuffix) {
+    StringBuffer sb = new StringBuffer();
+    Matcher matcher = JACK_FILE_EXTENSION_PATTERN.matcher(fileName.toString());
+    Preconditions.checkArgument(matcher.find(), "Expected .jack suffix in %s", fileName);
+    matcher.appendReplacement(sb, parserOutputXmlSuffix);
+    return sb.toString();
+  }
+
+  private static String extractInputLocation(String[] args) {
+    return Arrays.stream(args).collect(onlyElement());
   }
 }
